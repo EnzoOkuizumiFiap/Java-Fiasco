3bc8d88528fed61eb070a59f395217a8abeaf553
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2026-03-05 20:46:03 -0300

    Aula 04 - Dados primitivos

---
+++ JavaNinja/src/TipoDeDados/DadosPrimitivos.java
@@ -0,0 +1,18 @@
+package TipoDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /*Dados Primitivos
+        * Boolean,
+        * char,
+        * byte, short, int, long,
+        * float, double
+        * */
+
+        int idade = 16;
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        Long saldoBancaria = 9999999L;
+    }
+}
