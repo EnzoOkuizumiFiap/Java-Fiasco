10b99915ee455283994887e23af538dd70c3f3ff
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2026-07-29 22:13:08 -0300

    Aula 89 a 92 - Git Branch, Git Checkout, Git Push origin <nome_branch>, DTO x MAPPER, Merge para main

--- CadastroDeNinjas/src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaService.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/cadastrodeninjas/Ninjas/NinjaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /* #P83 22 - Ninja Service - JPA */
 @Service
@@ -12,21 +13,30 @@ public class NinjaService {
 
     @Autowired // Injeção de Dependência: o Spring cria e injeta o NinjaRepository nesta classe
     private NinjaRepository ninjaRepository;
+    @Autowired
+    private NinjaMapper ninjaMapper;
 
     // Listar todos os meus Ninjas / #P83 22 - Ninja Service - JPA
-    public List<NinjaModel> listarNinjas() {
-        return ninjaRepository.findAll();
+    public List<NinjaDTO> listarNinjas() {
+        List<NinjaModel> ninjas = ninjaRepository.findAll();
+
+        return ninjas.stream()
+                .map(ninjaMapper::map)
+                .collect(Collectors.toList());
     }
 
     // Listar o meu ninja por ID / #P84 23 - Ninja Service - Listar Ninja + Serialização
-    public NinjaModel listarNinjaPorId(Long id) {
+    public NinjaDTO listarNinjaPorId(Long id) {
         Optional<NinjaModel> ninjaPorId = ninjaRepository.findById(id);
-        return ninjaPorId.orElse(null);
+
+        return ninjaPorId.map(ninjaMapper::map).orElse(null);
     }
 
     // Criar um novo ninja / #P85 24 - Ninja Service - Criando ninjas
-    public NinjaModel criarNinja(NinjaModel ninja) {
-        return ninjaRepository.save(ninja);
+    public NinjaDTO criarNinja(NinjaDTO ninjaDTO) {
+        NinjaModel ninjaModel = ninjaMapper.map(ninjaDTO);
+        ninjaModel = ninjaRepository.save(ninjaModel);
+        return ninjaMapper.map(ninjaModel);
     }
 
     // Deletar o ninja (Tem que ser um metodo VOID) / #P87 26 - Ninja Service - Deletar ninja
@@ -35,10 +45,14 @@ public class NinjaService {
     }
 
     // Atualizar Ninja / #P88 27 - Ninja Service - Atualizar Ninja
-    public NinjaModel atualizarNinja(Long id, NinjaModel ninjaAtualizado) {
-        if (ninjaRepository.existsById(id)) {
+    public NinjaDTO atualizarNinja(Long id, NinjaDTO ninjaDTO) {
+        Optional<NinjaModel> ninjaExistente = ninjaRepository.findById(id);
+        if (ninjaExistente.isPresent()) {
+            NinjaModel ninjaAtualizado = ninjaMapper.map(ninjaDTO);
             ninjaAtualizado.setId(id);
-            return ninjaRepository.save(ninjaAtualizado);
+            NinjaModel ninjaSalvo = ninjaRepository.save(ninjaAtualizado);
+
+            return ninjaMapper.map(ninjaSalvo);
         }
         return null;
     }
